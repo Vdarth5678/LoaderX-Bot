b1cb28ff5ee05dfc632c4109154055ff7c83b77e
Vdarth5678 <68529552+Vdarth5678@users.noreply.github.com>
2020-11-23 07:20:38 +0530

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -5,6 +5,7 @@ aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 \
   --max-connection-per-server=10 --rpc-max-request-size=1024M \
   --bt-tracker="[$tracker_list]" --bt-max-peers=0 --seed-time=0.01 \
   --min-split-size=10M --follow-torrent=mem --split=10 \
-   --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED \
-   --max-overall-upload-limit=1K --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS
-   --peer-id-prefix=-TR3000- --user-agent=Transmission/3.00
+  --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED \
+  --max-overall-upload-limit=1K --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS
+  --peer-id-prefix=-qB4250- --user-agent==qBittorrent/4.2.5
+  
